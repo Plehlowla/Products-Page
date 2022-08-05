8bd009c5b7b386935c5f08a33b3a7c6ba57ff6ef
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-08-05 08:16:22 -0500

    loading / error innerHTML

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const url = "https://course-api.com/javascript-store-products";
+
+const productsDOM = document.querySelector(".products-center");
+
+const fetchProduct = async () => {
+  productsDOM.innerHTML = `<div class="loading"></div>`;
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    console.log(data);
+  } catch (error) {
+    productsDOM.innerHTML = `<p class="error">There was an error</p>`;
+  }
+};
+
+fetchProduct();
