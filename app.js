73e088aa02e68c7b3efc27a6c4678aee7ce5947a
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-08-24 18:59:11 -0500

    display product function

--- app.js
+++ app.js
@@ -14,7 +14,30 @@ const fetchProduct = async () => {
 };
 
 const displayProduct = (list) => {
-  console.log(list);
+  // console.log(list);
+  const productList = list
+    .map((product) => {
+      const { id } = product;
+      // console.log(id);
+      const { name: title, price } = product.fields;
+      // console.log(name, price);
+      const { url: img } = product.fields.image[0];
+      // console.log(url);
+      const formatPrice = price / 100;
+      // id,name,price,img
+      return `<a class="single-product" href="product.html">
+            <img src="${img}" class="single-product-img img" alt="${title}" />
+            <footer>
+              <h5 class="name">${title}</h5>
+              <span class="price">$${formatPrice}</span>
+            </footer>
+          </a>`;
+    })
+    .join("");
+
+  productsDOM.innerHTML = `        <div class="products-container">
+        ${productList}
+        </div>`;
 };
 
 const start = async () => {
