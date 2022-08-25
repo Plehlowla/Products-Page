890b7dbfdb3ec7106c9e122f4d3cea6c6ea2724e
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-08-24 20:18:45 -0500

    fetch product, dynamic params

--- products.js
+++ products.js
@@ -1,10 +1,19 @@
 const productDOM = document.querySelector(".product");
 // console.log(productDOM);
-const url =
-  "https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog";
+const url = "https://course-api.com/javascript-store-single-product";
 
 const fetchProduct = async () => {
-  return "test";
+  try {
+    productDOM.innerHTML = "<h4 class='product-loading'>Loading...</h4>";
+    // console.log(window.location.search);
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+    const response = await fetch(`${url}?id=${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    productDOM.innerHTML = "<p class='error'>Beep Boop Error Detected</p>";
+  }
 };
 
 const displayProduct = (product) => {
